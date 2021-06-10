14363b75fdb50cb21da53b90d8319184ef5e2009
profnike <profnike@gmail.com>
2021-06-10 22:31:01 +0100

    Adding class work file

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+
+    function in1To10(n,outsideMode){
+  
+
+        if((n>=10||n<=1  )&&(outsideMode===true)){
+         
+         
+         return true;
+         
+         
+         }
+         
+         
+         else if ((n<=10 && n>=1)&&(outsideMode===false)){
+       
+         return true;
+         
+         }
+         
+         
+         
+         else{return false;}
+     }
